f8f5daa0045e2448f3335b9ee9405dd29536ba6f
tectrixer <tonihoevedes@gmail.com>
2019-06-16 13:56:42 +0200

    added comment

--- convert.py
+++ convert.py
@@ -1,2 +1,3 @@
+# make sure that the converting.py file is in the same directory
 import converting
 print(converting.conv_a_to_a(input("The number you want to convert: "), input("The number system of your number: "), input("The number system of the output number: ")))
