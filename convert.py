d47edc7ae6cedd9bce1cbf430ef7622e012edc22
tectrixer <tonihoevedes@gmail.com>
2019-06-16 13:49:59 +0200

    Added the final convert.py file

---
+++ convert.py
@@ -0,0 +1,2 @@
+import converting
+print(converting.conv_a_to_a(input("The number you want to convert: "), input("The number system of your number: "), input("The number system of the output number: ")))
