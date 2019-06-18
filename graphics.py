ebf3bbf9638a3701ea5b9f430ae7babcb240cc5f
tectrixer <tonihoevedes@gmail.com>
2019-06-18 21:52:32 +0200

    tkinter test

--- graphics.py
+++ graphics.py
@@ -1,5 +1,12 @@
 from tkinter import *
+clicked = False
+def click():
+    if clicked == False:
+        Label(screen, text= "Clicked")
+    else:
+        Label(screen, text= "Unclicked")
 screen = Tk()
 screen.title("Converting Numbers")
 Label(screen, text="Welcome to this program").pack()
+Button(screen, text="Press Me", command=click).pack(fill=BOTH)
 screen.mainloop()
